beb6f6e4ddc7f950c852a37eb69b7d11768eb76f
will-344 <willmartin344@gmail.com>
2025-09-22 13:23:15 -0400

    add module for analytical solutions

---
+++ Project1/analytical.py
@@ -0,0 +1,11 @@
+""" Module that provides the analytic solution to the following problems, with
+necessary constants taken from function inputs:
+
+ode1 (ODE): Steady 1D cylindrical heat conduction with no energy generation,
+    assuming constant thermal conductivity, with specified temperature boundary
+    conditions.
+     
+int1 (definite integral): Elliptical lift distribution for a finite wing,
+    assuming incompressible, inviscid, and steady flow, using lifting-line
+    theory in a uniform freestream with constant density.
+"""
