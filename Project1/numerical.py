3c3fbcd31452874568dc06ddbb0cb4b8f5b26da8
will-344 <willmartin344@gmail.com>
2025-09-22 16:02:38 -0400

    add function for rk4 method for solving ODE

--- Project1/numerical.py
+++ Project1/numerical.py
@@ -26,3 +26,34 @@ def euler(temp1, temp2, rad1, rad2, d_rad):
     
     temp = x1
     return temp, rad
+
+def rk4(temp1, temp2, rad1, rad2, d_rad):
+    x1 = np.array([temp1])
+    x2 = np.array([(temp2-temp1) / (rad1 * np.log(rad2/rad1))])
+    iterations = round((rad2-rad1) / d_rad)
+    rad = np.linspace(rad1, rad2, iterations)
+    
+    for i in range(iterations-1):
+        k1x1 = x2[i]
+        k1x2 = -x2[i] / rad[i]
+        
+        x2a = x2[i] + k1x2*d_rad/2
+        k2x1 = x2a
+        k2x2 = -x2a / (rad[i] + d_rad/2)
+        
+        x2b = x2[i] + k2x2*d_rad/2
+        k3x1 = x2b
+        k3x2 = -x2b / (rad[i] + d_rad/2)
+        
+        x2c = x2[i] + k3x2*d_rad
+        k4x1 = x2c
+        k4x2 = -x2c / (rad[i] + d_rad)
+        
+        x1new = x1[i] + (k1x1 + 2*k2x1 + 2*k3x1 + k4x1)*d_rad/6
+        x2new = x2[i] + (k1x2 + 2*k2x2 + 2*k3x2 + k4x2)*d_rad/6
+        
+        x1 = np.append(x1, x1new)
+        x2 = np.append(x2, x2new)
+    
+    temp = x1
+    return temp, rad
